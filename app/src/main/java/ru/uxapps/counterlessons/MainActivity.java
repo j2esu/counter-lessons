7571f52ae9b3c2470c5005fb0502ac6ed21cbecd
Evgeny Egorov <ipegorovev@gmail.com>
2019-02-21 11:58:13 +0300

    Lesson 3 - finish

--- app/src/main/java/ru/uxapps/counterlessons/MainActivity.java
+++ app/src/main/java/ru/uxapps/counterlessons/MainActivity.java
@@ -1,43 +1,43 @@
 package ru.uxapps.counterlessons;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREF_VAL = "PREF_VAL";
+
     private TextView mValueTv;
-    private int mValue;
+    private SharedPreferences mPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         mValueTv = findViewById(R.id.value);
-        findViewById(R.id.plus).setOnClickListener(v -> {
-            mValue++;
-            updateValue();
-        });
-        findViewById(R.id.minus).setOnClickListener(v -> {
-            mValue--;
-            updateValue();
-        });
-
+        updateValue(getValue());
+        findViewById(R.id.plus).setOnClickListener(v -> updateValue(getValue() + 1));
+        findViewById(R.id.minus).setOnClickListener(v -> updateValue(getValue() - 1));
         findViewById(R.id.reset).setOnClickListener(v -> {
-            int oldValue = mValue;
-            mValue = 0;
-            updateValue();
+            int oldValue = getValue();
+            updateValue(0);
             Snackbar.make(v, "Counter was reset", Snackbar.LENGTH_SHORT)
-                    .setAction("Undo", ignored -> {
-                        mValue = oldValue;
-                        updateValue();
-                    })
+                    .setAction("Undo", ignored -> updateValue(oldValue))
                     .show();
         });
     }
 
-    private void updateValue() {
-        mValueTv.setText(String.valueOf(mValue));
+    private void updateValue(int newValue) {
+        mPrefs.edit().putInt(PREF_VAL, newValue).apply();
+        mValueTv.setText(String.valueOf(newValue));
+    }
+
+    private int getValue() {
+        return mPrefs.getInt(PREF_VAL, 0);
     }
 }
