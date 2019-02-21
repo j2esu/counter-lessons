01fd8c53b7fa0ec7b255ec405ce664a32621eafa
Evgeny Egorov <ipegorovev@gmail.com>
2019-02-21 09:56:39 +0300

    Lesson 2 - finish

--- app/src/main/java/ru/uxapps/counterlessons/MainActivity.java
+++ app/src/main/java/ru/uxapps/counterlessons/MainActivity.java
@@ -1,13 +1,43 @@
 package ru.uxapps.counterlessons;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mValueTv;
+    private int mValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mValueTv = findViewById(R.id.value);
+        findViewById(R.id.plus).setOnClickListener(v -> {
+            mValue++;
+            updateValue();
+        });
+        findViewById(R.id.minus).setOnClickListener(v -> {
+            mValue--;
+            updateValue();
+        });
+
+        findViewById(R.id.reset).setOnClickListener(v -> {
+            int oldValue = mValue;
+            mValue = 0;
+            updateValue();
+            Snackbar.make(v, "Counter was reset", Snackbar.LENGTH_SHORT)
+                    .setAction("Undo", ignored -> {
+                        mValue = oldValue;
+                        updateValue();
+                    })
+                    .show();
+        });
+    }
+
+    private void updateValue() {
+        mValueTv.setText(String.valueOf(mValue));
     }
 }
