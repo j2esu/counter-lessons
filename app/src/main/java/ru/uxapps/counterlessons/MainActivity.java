56d0dc03f412d4caaf90f5389998cb9eebab7dae
Evgeny Egorov <ipegorovev@gmail.com>
2019-03-08 11:15:28 +0300

    Homework done

--- app/src/main/java/ru/uxapps/counterlessons/MainActivity.java
+++ app/src/main/java/ru/uxapps/counterlessons/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_main);
 
-        List<Counter> data = new ArrayList<>();
-        data.add(new Counter("Counter 1", 123));
-        data.add(new Counter("Counter 2", 456));
-        data.add(new Counter("Counter 3", 789));
-
         CounterList list = new CounterList(findViewById(R.id.list));
-        list.setCounters(data);
+        list.setCounters(createTestData());
+    }
+
+    private List<Counter> createTestData() {
+        int count = 100;
+        Random random = new Random();
+        List<Counter> data = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            data.add(new Counter("Counter " + (i + 1), random.nextInt(1000)));
+        }
+        return data;
     }
 
 }
